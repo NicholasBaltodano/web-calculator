810e7d1d40231132e7c05b3252714a09f60680dc
nbaltodano <nicholasbaltodano@gmail.com>
2021-01-22 01:11:41 -0600

    operator and operand selection overhaul

--- script/drawCalculator.js
+++ script/drawCalculator.js
@@ -5,29 +5,70 @@ const numbers        = document.querySelectorAll('.number');
 const clearButton    = document.querySelector('#clear');
 const backspaceButton = document.querySelector('#backspace')
 const operators = document.querySelectorAll('.operator');
+const equalButton = document.querySelector('#equalButton');
+let   latestOperator = '';
+let   equalPressed   = false;
+let operator;
+let operand ;
+let operand2;
+
+
 // Events
 clearButton.addEventListener('click', clearScreen);
 backspaceButton.addEventListener('click', backspace);
 numbers.forEach( number => number.addEventListener('click', addText))
 operators.forEach( operator => operator.addEventListener('click', addHistory))
+equalButton.addEventListener('click', calculate);
 
 
 // Function Definitions
 function addText(e){
-    console.log(e)
-    mainScreen.innerText += e.srcElement.innerText; 
+    equalPressed = false;
+    if(mainScreen.textContent === "Something went wrong"){
+        mainScreen.textContent = '';
+    }
+    mainScreen.textContent += e.srcElement.textContent; 
 }
 
 function clearScreen(){
-    historyScreen.innerText = '';
-    mainScreen.innerText = '';
+    historyScreen.textContent = '';
+    mainScreen.textContent = '';
+    operator = '';
+    operand = '';
+    operand2 = '';
+    equalPressed = false;
 }
 
 function backspace(){
-    mainScreen.innerText = mainScreen.innerText.substring(0, mainScreen.innerText.length - 1 ); 
+    mainScreen.textContent = mainScreen.textContent.substring(0, mainScreen.textContent.length - 1 ); 
 }
 
-function addHistory(){
-    historyScreen.innerText = mainScreen.innerText;
-    mainScreen.innerText = '';
+function addHistory(e){
+
+    operator = e.srcElement.textContent;
+
+    if(mainScreen.textContent !== 'Something went wrong'){
+        operand  = Number(mainScreen.textContent);
+        historyScreen.textContent = operand + ' ' + operator;
+        mainScreen.textContent    = '';
+    }  
+}
+
+
+function calculate(){
+    
+    if(operator === ''){
+        mainScreen.textContent = 'Something went wrong';
+    }
+    else{
+
+        if(!equalPressed){
+            operand2 = Number(mainScreen.textContent);
+        }
+        
+        mainScreen.textContent = operate(operator, operand, operand2);
+        operand = Number(mainScreen.textContent);
+        equalPressed = true;
+    }
+   
 }
